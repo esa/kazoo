bdf524a1dbef881d522569758f54250e52765506
TASTE User <taste@tastevm.estec.esa.int>
2017-07-15 18:21:42 +0200

    Fix test script

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,2 @@
-../taste-aadl-parser -glue  -i test1/interfaceview.aadl -c test1/deploymentview.aadl -d test1/dataview.aadl ocarina_components.aadl TASTE_DV_Properties.aadl
-../taste-aadl-parser -gw  -i test2/InterfaceView.aadl -d test2/DataView.aadl TASTE_IV_Properties.aadl
+../aadl_parser -glue  -i test1/interfaceview.aadl -c test1/deploymentview.aadl -d test1/dataview.aadl ocarina_components.aadl TASTE_DV_Properties.aadl
+../aadl_parser -gw  -i test2/InterfaceView.aadl -d test2/DataView.aadl TASTE_IV_Properties.aadl

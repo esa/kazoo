027ef619ae3e7d489ae2d1a1a6a1a67b758b0575
Maxime Perrotin <Maxime.Perrotin@esa.int>
2017-11-27 15:01:23 +0100

    Remove old code from buildsupport

--- src/parser_version.ads
+++ src/parser_version.ads
@@ -1,6 +1,6 @@
 package Parser_Version is
    Parser_Release : constant String :=
-      "ae93668 ; Commit Date:   Sun Nov 26 21:38:55 2017 ";
+      "f39d2e0 ; Commit Date:   Mon Nov 27 13:42:45 2017 ";
    Ocarina_Version : constant String :=
       "Ocarina 2017.x (Working Copy from r0b92ed3)";
 end Parser_Version;

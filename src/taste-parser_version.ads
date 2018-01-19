6e3fa960ced0c39d6ff04a697e2473581f523847
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-01-19 17:21:22 +0100

    Start templates for C skeleton generation

--- src/taste-parser_version.ads
+++ src/taste-parser_version.ads
@@ -1,6 +1,6 @@
 package TASTE.Parser_Version is
    Parser_Release : constant String :=
-      "fd043de ; Commit Date:   Mon Jan 15 17:52:30 2018 ";
+      "b207577 ; Commit Date:   Tue Jan 16 15:55:38 2018 ";
    Ocarina_Version : constant String :=
       "Ocarina 2017.x (Working Copy from r0b92ed3)";
 end TASTE.Parser_Version;

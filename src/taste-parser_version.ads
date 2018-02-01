5f33616ffad703734909babe42712529fc3015c8
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-02-01 15:32:18 +0000

    Update templates to use STRIP

--- src/taste-parser_version.ads
+++ src/taste-parser_version.ads
@@ -1,6 +1,6 @@
 package TASTE.Parser_Version is
    Parser_Release : constant String :=
-      "177402f ; Commit Date:   Sat Jan 27 23:25:35 2018 ";
+      "39e5b1c ; Commit Date:   Thu Feb 1 15:29:17 2018 ";
    Ocarina_Version : constant String :=
       "Ocarina 2017.x (Working Copy from rd078c54)";
 end TASTE.Parser_Version;

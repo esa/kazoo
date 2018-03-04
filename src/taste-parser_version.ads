2b73475b95aee01739d94a7bd56acdd39b1ba8a5
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-03-04 11:25:16 +0100

    Revert Makefile

--- src/taste-parser_version.ads
+++ src/taste-parser_version.ads
@@ -1,6 +1,6 @@
 package TASTE.Parser_Version is
    Parser_Release : constant String :=
-      "ae9ef3d ; Commit Date:   Thu Mar 1 08:54:53 2018 ";
+      "1ccf1b2 ; Commit Date:   Sat Mar 3 21:28:23 2018 ";
    Ocarina_Version : constant String :=
-      "Ocarina 2017.x (Working Copy from r61c0f39)";
+      "Ocarina 2017.x (Working Copy from rc50d394)";
 end TASTE.Parser_Version;

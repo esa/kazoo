bb92976faf10eec1e2a1ecc48dd357d38612a301
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-03-14 16:09:08 +0100

    Removed need to pass TASTE_IV/DV_Properties.aadl
    Files are now part of Ocarina library

--- src/taste-parser_version.ads
+++ src/taste-parser_version.ads
@@ -1,6 +1,6 @@
 package TASTE.Parser_Version is
    Parser_Release : constant String :=
-      "5a9ad95 ; Commit Date:   Wed Mar 14 14:09:23 2018 ";
+      "8248f43 ; Commit Date:   Wed Mar 14 16:04:45 2018 ";
    Ocarina_Version : constant String :=
-      "Ocarina 2017.x (Working Copy from r957625a)";
+      "Ocarina 2017.x (Working Copy from r2a52334)";
 end TASTE.Parser_Version;

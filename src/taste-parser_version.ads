f3ebf7555811ceee43de42a34fa601949ca9ba09
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-03-17 17:12:31 +0100

    SDL header backend: add missing timer

--- src/taste-parser_version.ads
+++ src/taste-parser_version.ads
@@ -1,6 +1,6 @@
 package TASTE.Parser_Version is
    Parser_Release : constant String :=
-      "1505596 ; Commit Date:   Sat Mar 17 09:33:22 2018 ";
+      "8fd8111 ; Commit Date:   Sat Mar 17 11:05:10 2018 ";
    Ocarina_Version : constant String :=
       "Ocarina 2017.x (Working Copy from r2a52334)";
 end TASTE.Parser_Version;

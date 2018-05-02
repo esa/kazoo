5ae63971e3107dce7807941866dc57bbd8551a66
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-05-02 15:25:10 +0200

    Remove file autogenerated during build

--- src/taste-parser_version.ads
+++
@@ -1,6 +0,0 @@
-package TASTE.Parser_Version is
-   Parser_Release : constant String :=
-      "a7aa20a ; Commit Date:   Sat Apr 28 14:13:36 2018 ";
-   Ocarina_Version : constant String :=
-      "Ocarina 2017.x (Working Copy from r59b19f4)";
-end TASTE.Parser_Version;

5b68ef0811582d687dce939070cf6b957686d560
Maxime Perrotin <Maxime.Perrotin@esa.int>
2019-08-09 12:14:53 +0100

    Add test case with more than one port

---
+++ test/test-air-multiports/input_data/read.c
@@ -0,0 +1,19 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "read.h"
+
+void read_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+}
+
+void read_PI_h()
+{
+    static asn1SccT_Boolean someBool = true;
+    /* Write your code here! */
+    printf("read_RI_s is sending %s\n", someBool?"TRUE":"FALSE");
+    read_RI_s(&someBool);
+    someBool = someBool?false:true;
+}
+

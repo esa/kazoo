d780fd94f1ac276bc6d3317b67220126d5730295
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-08-09 11:13:31 +0200

    Update documentation

--- doc/to_html.sh
+++ doc/to_html.sh
@@ -1,1 +1,3 @@
 pandoc -c kazoo.css -f mediawiki -t html5 -s updated.mediawiki -o test.html
+# To generate text documentation with nice tables:
+pandoc -c ../kazoo.css -f mediawiki -t rst+grid_tables --toc -o doc.txt templates_from_wiki

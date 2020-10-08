85eec3bbea1d2e7a7b77a0b46d15e560eb9675ac
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-10-08 11:12:13 +0200

    Add regression script

---
+++ test/ri_during_startup/regression.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+timeout = 5
+
+binaries = [
+    "work/binaries/demo"
+]
+
+expected = [
+    "[TASTE] Initialization completed for function demo_Timer_Manager",
+    "[MASTER] Set timer and call Slave during init",
+    "[TASTE] Initialization completed for function master",
+    "[slave] Startup",
+    "[TASTE] Initialization completed for function slave",
+    "[slave] I am free? Really? I don't believe it",
+    "[MASTER] Timer expired, Slave did not answer within 1 sec (as expected)",
+    "[MASTER] Trying again",
+    "[slave] Second call, I say thank you",
+    "[MASTER] Got Answer from slave (should be after 2nd call)",
+    "You may now press Ctrl-C to stop the application"
+]
+
+sys.path.append("..")
+import commonRegression
+result = commonRegression.test(binaries, expected, timeout)
+sys.exit(result)

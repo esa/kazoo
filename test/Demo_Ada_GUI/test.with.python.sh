ca7ef33ab1d94a0991bc9575f87ffb404f5634b1
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-10-08 11:39:18 +0200

    Update test cases

--- test/Demo_Ada_GUI/test.with.python.sh
+++ test/Demo_Ada_GUI/test.with.python.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-if [ ! -d output.pohic/binaries/ ] ; then
+if [ ! -d work/binaries/ ] ; then
 	echo Build first - invoke make
 	exit 1
 fi
-echo Run 
-echo "    " export ASSERT_IGNORE_GUI_ERRORS=1
-echo from another shell, and hit ENTER - Stop the test with Ctrl-C
-read ANS
-cp Test_TM_TC_Demo_Ada_GUI_2.py output.pohic/binaries/mygui_GUI/
+#   echo Run 
+#   echo "    " export ASSERT_IGNORE_GUI_ERRORS=1
+#   echo from another shell, and hit ENTER - Stop the test with Ctrl-C
+#   read ANS
+export ASSERT_IGNORE_GUI_ERRORS=1
+cp Test_TM_TC_Demo_Ada_GUI_2.py work/binaries/mygui_GUI/
 # Test C version
-cd output.pohic/binaries/mygui_GUI/
+cd work/binaries/mygui_GUI/
 ../mypartition &
-python Test_TM_TC_Demo_Ada_GUI_2.py
+python3 Test_TM_TC_Demo_Ada_GUI_2.py
 pkill mypartition
 cd -
